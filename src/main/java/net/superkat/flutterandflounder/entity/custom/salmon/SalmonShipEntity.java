796e37d4566c6da8e3eead90abd129dff516e350
Superkat32 <superkat1806@gmail.com>
2023-12-20 23:19:55 -0500

    Salmon Ship - oh my gosh this was really hard and frustrating and time-consuming
    FEAT: Added the Salmon Ship's AI, it body slams you.

--- src/main/java/net/superkat/flutterandflounder/entity/custom/salmon/SalmonShipEntity.java
+++ src/main/java/net/superkat/flutterandflounder/entity/custom/salmon/SalmonShipEntity.java
@@ -1,13 +1,14 @@
 package net.superkat.flutterandflounder.entity.custom.salmon;
 
+import net.minecraft.block.BlockState;
 import net.minecraft.entity.EntityType;
-import net.minecraft.entity.ai.goal.ActiveTargetGoal;
 import net.minecraft.entity.ai.goal.LookAtEntityGoal;
 import net.minecraft.entity.attribute.DefaultAttributeContainer;
 import net.minecraft.entity.attribute.EntityAttributes;
 import net.minecraft.entity.mob.HostileEntity;
 import net.minecraft.entity.mob.MobEntity;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import net.superkat.flutterandflounder.entity.custom.CommonBossFish;
 import net.superkat.flutterandflounder.entity.goals.BossGoals;
@@ -21,25 +22,43 @@ import software.bernie.geckolib.util.GeckoLibUtil;
 
 public class SalmonShipEntity extends HostileEntity implements CommonBossFish {
     protected static final RawAnimation IDLE_ANIM = RawAnimation.begin().thenLoop("animation.salmonship.idle");
-    protected static final RawAnimation WARNING_ANIM = RawAnimation.begin().thenPlayAndHold("animation.salmonship.warning");
+    protected static final RawAnimation WARNING_ANIM = RawAnimation.begin().thenPlay("animation.salmonship.warning");
     protected static final RawAnimation ATTACK_ANIM = RawAnimation.begin().thenPlayAndHold("animation.salmonship.attack");
-    protected static final RawAnimation RAISE_ANIM = RawAnimation.begin().thenPlayAndHold("animation.salmonship.raise");
+    protected static final RawAnimation FLOUNDER_ANIM = RawAnimation.begin().thenLoop("animation.salmonship.flounder");
+    private boolean isWarning = false;
+    //2 seconds
+    public int warningTicks = 40;
+    public boolean warningAnimInProgress = false;
+    public boolean shouldAttack = false;
+    public boolean playWarningAnim = false;
+
     private final AnimatableInstanceCache geoCache = GeckoLibUtil.createInstanceCache(this);
     public SalmonShipEntity(EntityType<? extends HostileEntity> entityType, World world) {
         super(entityType, world);
+        this.moveControl = new BossGoals.SalmonShipMoveControls(this, 4);
     }
 
     @Override
     public void registerControllers(AnimatableManager.ControllerRegistrar controllers) {
-        controllers.add(new AnimationController<>(this, "controller", 5, this::animController));
+        controllers.add(
+                new AnimationController<>(this, "controller", 5, this::animController),
+                new AnimationController<>(this, "warning", 5, this::warningController)
+
+        );
     }
 
-    protected<E extends SalmonShipEntity> PlayState animController(final AnimationState<E> event) {
-        if(!event.isMoving()) {
-            return event.setAndContinue(IDLE_ANIM);
-        } else {
-            return PlayState.STOP;
+    protected PlayState animController(final AnimationState<SalmonShipEntity> event) {
+        if(this.isAttacking()) {
+            return event.setAndContinue(FLOUNDER_ANIM);
         }
+        return event.setAndContinue(IDLE_ANIM);
+    }
+
+    protected PlayState warningController(final AnimationState<SalmonShipEntity> event) {
+        if(this.isAttacking()) {
+            return event.setAndContinue(WARNING_ANIM);
+        }
+        return event.setAndContinue(IDLE_ANIM);
     }
 
     @Override
@@ -52,7 +71,7 @@ public class SalmonShipEntity extends HostileEntity implements CommonBossFish {
                 .add(EntityAttributes.GENERIC_MAX_HEALTH, 15)
                 .add(EntityAttributes.GENERIC_MOVEMENT_SPEED, 0.3)
                 .add(EntityAttributes.GENERIC_ATTACK_KNOCKBACK, 3)
-                .add(EntityAttributes.GENERIC_ATTACK_DAMAGE, 20)
+                .add(EntityAttributes.GENERIC_ATTACK_DAMAGE, 25)
                 .add(EntityAttributes.GENERIC_FOLLOW_RANGE, 48);
     }
 
@@ -60,6 +79,52 @@ public class SalmonShipEntity extends HostileEntity implements CommonBossFish {
     protected void initGoals() {
         this.goalSelector.add(1, new BossGoals.SlamPlayerGoal(this));
         this.goalSelector.add(2, new LookAtEntityGoal(this, PlayerEntity.class, 15f, 1f));
-        this.targetSelector.add(1, new ActiveTargetGoal<>(this, PlayerEntity.class, true));
+        this.targetSelector.add(1, new BossGoals.BossFindTargetGoal(this));
+    }
+
+    @Override
+    protected void fall(double heightDifference, boolean onGround, BlockState state, BlockPos landedPosition) {
+    }
+
+
+    @Override
+    public void tick() {
+        super.tick();
+        if(isWarning) {
+            warningTicks--;
+            if(warningTicks <= 0) {
+                isWarning = false;
+                warningAnimInProgress = false;
+                shouldAttack = true;
+                warningTicks = 40;
+            }
+        }
+    }
+
+    public void setWarningTicks(int warningTicks) {
+        this.warningTicks = warningTicks;
+    }
+
+    public void setIsWarning(boolean warning) {
+        this.isWarning = warning;
+        if(warning) {
+            playWarningAnim = true;
+        }
+    }
+
+    public boolean isWarning() {
+        return this.isWarning;
+    }
+
+    public int getWarningTicks() {
+        return warningTicks;
+    }
+
+    public boolean isShouldAttack() {
+        return shouldAttack;
+    }
+
+    public void setShouldAttack(boolean shouldAttack) {
+        this.shouldAttack = shouldAttack;
     }
 }
